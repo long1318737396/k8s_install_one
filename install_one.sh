f2ef2ce0aed59b82c7515f6b3dbd45960350ec0b
pangguanglong <pangguanglong@360.cn>
2025-05-17 00:46:25 +0800

    更新readme

--- install_one.sh
+++ install_one.sh
@@ -287,12 +287,12 @@ tar -zxvf etcd-${etcd_version}-linux-${ARCH}.tar.gz -C ${bin_dir} --strip-compon
 
 chmod +x ${bin_dir}/etcd*
 
-tar zxvf nerdctl-full-${nerdctl_full_version}-linux-${ARCH}.tar.gz -C ${bin_dir}
-/bin/cp ${bin_dir}/lib/systemd/system/*.service /etc/systemd/system/
+tar zxvf nerdctl-full-${nerdctl_full_version}-linux-${ARCH}.tar.gz -C /usr/local/bin
+/bin/cp /usr/local/bin/lib/systemd/system/*.service /etc/systemd/system/
 mkdir -p /opt/cni/bin
-/bin/cp ${bin_dir}/libexec/cni/* /opt/cni/bin/
-sed -i "s@/usr/local/bin@${bin_dir}@g" /etc/systemd/system/buildkit.service
-sed -i "s@/usr/local/bin@${bin_dir}@g" /etc/systemd/system/containerd.service
+/bin/cp /usr/local/bin/libexec/cni/* /opt/cni/bin/
+#sed -i "s@/usr/local/bin@${bin_dir}@g" /etc/systemd/system/buildkit.service
+#sed -i "s@/usr/local/bin@${bin_dir}@g" /etc/systemd/system/containerd.service
 
 systemctl enable buildkit containerd 
 systemctl start buildkit containerd 
