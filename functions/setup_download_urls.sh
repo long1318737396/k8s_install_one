849adb391168db95c35cc91443d339e4fe08f886
pangguanglong <pangguanglong@360.cn>
2025-11-19 11:03:07 +0800

    网络插件下载

--- functions/setup_download_urls.sh
+++ functions/setup_download_urls.sh
@@ -24,6 +24,8 @@ setup_download_urls() {
   crio_url="https://storage.googleapis.com/cri-o/artifacts/cri-o.${ARCH}.v${crio_version}.tar.gz"
   cri_docker_url="https://github.com/Mirantis/cri-dockerd/releases/download/v${cri_docker_version}/cri-dockerd-${cri_docker_version}.${ARCH}.tgz"
   helm_url="https://get.helm.sh/helm-v${helm_version}-linux-${ARCH}.tar.gz"
-  containernetworking="https://github.com/containernetworking/plugins/releases/download/v${containernetworking}/cni-plugins-linux-${ARCH}-v${containernetworking}.tgz"
+  containernetworking="https://github.com/containernetworking/plugins/releases/download/v${containernetworking_version}/cni-plugins-linux-${ARCH}-v${containernetworking_version}.tgz"
   echo "Download URLs setup completed."
 }
+
+
